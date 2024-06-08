f971793194098265a1390b5ad3a69d7b2bc079eb
Mario Gonzalez Ruiz <mariogo2@c2r6s2.42malaga.com>
2024-06-08 18:21:58 +0200

    ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,35 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int count_digits(int n)
+long int ft_count_digits(long int n)
 {
-    int ndigits;
+    long int ndigits;
     ndigits = 0;
+
+    if (n == 0)
+        return 1;
+    if (n < 0)
+    {
+        ndigits++;
+        n = -n;
+    }
 
     while (n > 0)
     {
-        
+        n /= 10;    
+        ndigits++;
     }
     return ndigits;
 }
-int  ft_itoa(int n)
+
+char *ft_itoa(int n)
 {
+    char *char_arr;
+    int i;
+    long int lng_n;
 
-    char *reversed_arr;
-    int i = 0;
+    lng_n = n;
+    i = ft_count_digits(lng_n);
 
-    while (n > 0)
+    char_arr  = (char *)malloc(sizeof(char) * ft_count_digits(lng_n) + 1);
+    if (!char_arr)
+        return (NULL);
+
+    char_arr[i--] = '\0';
+    if (lng_n == 0)
+        char_arr[i--] = '0';
+    if (lng_n < 0)
     {
-        // reversed_arr[i] = (n % 10) + '0';
-        printf("%d\n", n % 10);
-        n = n / 10;
-        i++;
-    
+        char_arr[0] = '-';
+        lng_n = -lng_n;
     }
-    return i;
+    while (lng_n > 0)
+    {
+        char_arr[i--] = (lng_n % 10) + '0';
+        lng_n = lng_n / 10;
+    }
+    return char_arr;
 }
 
-int main(void)
-{
-    printf("%d\n", ft_itoa(12345));
+// int main(void)
+// {
+//     printf("%s\n", ft_itoa(0));
+//     // printf("%ld\n", ft_count_digits(0));
 
-}
+// }
