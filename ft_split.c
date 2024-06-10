2b26a38536b912445119179c3e28a2a6fdd9d846
Mario Gonzalez Ruiz <mariogo2@c1r11s3.42malaga.com>
2024-06-10 15:23:10 +0200

    idk

--- ft_split.c
+++ ft_split.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 #include <stdio.h>
-int count_words(char const *s, char delim)
+int ft_count_words(char const *s, char delim)
 {
 	int i;
 	int count;
@@ -23,13 +23,50 @@ int count_words(char const *s, char delim)
 		count--;
 	return count;
 }
-char			**ft_split(char const *s, char c)
-{
 
+void ft_copy_sub_to_str(const char *pos, char delim, char *substr)
+{
+	int i = 0;
+	while (pos[i] != delim && pos[i])
+	{
+		substr[i] = pos[i];
+		i++;
+	}
+	substr[i] = '\0';
 }
 
-int main(void)
+char	**ft_split(char const *s, char c)
 {
-	printf("%d\n", count_words("hello      world      a ", ' '));
-	return 0;
+	int i;
+	char **p_to_strs;
+	int count;
+	int size_to_malloc;
+	
+	count = 0;
+	i = 0;
+	p_to_strs = malloc(sizeof(char *) * ft_count_words(s, c) + 1);
+	while (s[i])
+	{
+		while (s[i] == c && s[i])
+			i++;
+		size_to_malloc = 0;
+		while (s[i] != c && s[i])
+		{
+			size_to_malloc++;
+			i++;
+		}
+		p_to_strs[count] = malloc(sizeof(char) * size_to_malloc + 1);
+		ft_copy_sub_to_str(&s[i - size_to_malloc], c, p_to_strs[count++]);
+	}
+	return p_to_strs;
 }
+
+
+// int main(void)
+// {
+// 	// printf("%d\n", ft_count_words("  hello world     ", ' '));
+// 	char *large_str = "hello world     this is a test    ";
+// 	char **substrs = ft_split(large_str, ' ');
+// 	// printf("%s\n", substrs[1]);
+// 	return 0;
+// }
