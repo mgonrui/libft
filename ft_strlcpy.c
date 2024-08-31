12af91853640e46a02c591aafaacd42893abe124
mgr <mgr@localhost.localdomain>
2024-08-31 20:24:09 +0200

    strlcpy done

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,7 +1,20 @@
 
 #include "libft.h"
-size_t strlcpy(char *dst, const char *src, size_t dstsize)
+
+size_t ft_strlcpy(char *dest, char *src, size_t size)
 {
+    size_t i;
     
+    if (size == 0)
+        return (ft_strlen(src));
+    i = 0;
+    while (src[i] != '\0' &&  i < size - 1) 
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+
+    return (ft_strlen(src));
 }
         
